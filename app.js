10d95b7022218d5ae97104de790e70a94fa999f5
jes708 <jonschwarz@nyu.edu>
2016-07-09 12:40:46 -0400

    refactor

--- app.js
+++ app.js
@@ -1,15 +1,171 @@
+'use strict';
+
 var express = require('express');
-var bodyParser = require('body-parser');
-var bookRoute = require('./api/books');
-var session = require('express-session');
+
 var app = express();
+var bodyParser = require('body-parser');
+var session = require('express-session');
 
-app.use('/files', express.static('public/static'))
+var path = require('path')
+var models = require('./models'),
+    Author = models.Author,
+    Book = models.Book,
+    Chapter = models.Chapter,
+    db = models.db;
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }))
 
-app.use('/api/books', bookRoute);
+app.use(bodyParser.json())
+
+app.use('/files', express.static(path.join(__dirname + '/public/static')));
+
+app.get('/api/books/', function(req, res, next){
+  if (req.query.title) {
+    Book.findAll({
+      where: {
+        title: req.query.title
+      }
+    }).then(function(books){
+    res.json(books)
+  }).catch(next)
+  } else {
+    Book.findAll({})
+    .then(function(books){
+      res.json(books)
+    }).catch(next)
+  }
+})
+
+app.post('/api/books/', function(req, res, next){
+  Book.create({
+    title: req.body.title,
+    authorId: req.body.authorId
+  })
+  .then(function(book){
+    res.status(201).json(book)
+  }).catch(next)
+})
+
+app.get('/api/books/:id', function(req, res, next){
+  var id = req.params.id;
+  Book.findOne({
+    where: {
+      id: id
+    }
+  }).then(function(book) {
+    if (!book) res.sendStatus(404);
+    res.json(book)
+  }).catch(next)
+})
+
+app.put('/api/books/:id', function(req, res, next){
+  var id = req.params.id;
+  Book.findOne({
+    where: {
+      id: id
+    }
+  }).then(function(book){
+    if (!book) res.sendStatus(404);
+    return book.update({
+      title: req.body.title
+    })
+  }).then(function(updatedBook) {
+    res.json(updatedBook)
+  }).catch(next)
+})
+
+app.delete('/api/books/:id', function(req, res, next){
+  var id = req.params.id;
+  Book.destroy({
+    where: {
+      id: id
+    }
+  }).then(function(book) {
+    if (!book) res.sendStatus(404);
+    res.sendStatus(204);
+  }).catch(next)
+})
+
+app.get('/api/books/:id/chapters', function(req, res, next){
+  var id = req.params.id;
+  Chapter.findAll({
+    where: {
+      bookId: id
+    }
+  }).then(function(chapters) {
+    if (!chapters) res.sendStatus(404);
+    res.json(chapters)
+  }).catch(next)
+})
+
+app.post('/api/books/:id/chapters', function(req, res, next){
+  var id = req.params.id;
+
+  Chapter.create({
+    title: req.body.title,
+    text: req.body.text,
+    number: req.body.number
+  }).then(function(chapter) {
+    Book.findOne({
+      where: {
+        id: id
+      }
+    }).then(function(book) {
+      return chapter.setBook(book)
+    }).then(function(updatedChapter){
+      res.status(201).json(updatedChapter);
+    }).catch(next)
+  }).catch(next)
+
+})
+
+app.get('/api/books/:bookId/chapters/:chapId', function(req, res, next){
+  var bookId = req.params.bookId;
+  var chapId = req.params.chapId;
+  Chapter.findOne({
+    where: {
+      bookId: bookId,
+      id: chapId
+    }
+  }).then(function(chapter) {
+    if (!chapter) res.sendStatus(404);
+    res.json(chapter)
+  }).catch(next)
+})
+
+app.put('/api/books/:bookId/chapters/:chapId', function(req, res, next){
+  var bookId = req.params.bookId;
+  var chapId = req.params.chapId;
+  Chapter.findOne({
+    where: {
+      bookId: bookId,
+      id: chapId
+    }
+  }).then(function(chapter){
+    if (!chapter) res.sendStatus(404);
+    return chapter.update({
+      title: req.body.title
+    })
+  }).then(function(updatedChapter) {
+    res.json(updatedChapter)
+  }).catch(next)
+})
+
+app.delete('/api/books/:bookId/chapters/:chapId', function(req, res, next){
+  var bookId = req.params.bookId;
+  var chapId = req.params.chapId;
+  Chapter.destroy({
+    where: {
+      bookId: bookId,
+      id: chapId
+    }
+  }).then(function(chapter){
+    if (!chapter) res.sendStatus(404);
+    res.sendStatus(204);
+  }).catch(next)
+})
+
+module.exports = app;
 
 app.get('/broken', function(req, res, next){
   res.sendStatus(500)
@@ -21,13 +177,10 @@ app.get('/forbidden', function(req, res, next){
 
 app.use(session({
   genid: function(req) {
-    return req.headers.host // use UUIDs for session IDs
+    return req.headers.host
   },
-  // secret: 'dougFunny',
   cookie: {},
   secret: 'dougFunny'
-  // resave: false,
-  // saveUninitialized: true
 }))
 
 app.get('/api/numVisits', function(req, res, next){
